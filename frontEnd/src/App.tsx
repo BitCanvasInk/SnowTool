94d807f372b44479935af65edd5c701cdb279be4
justbraf <dwaynecorey@gmail.com>
2025-07-12 13:58:33 -0400

    feat switch states & backend initial framework

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
     <>
-    <p className="text-9xl">landing</p>
+      <p className="text-5xl">SnowBall Finance</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      // <Login />
     </>
   )
 }
---
+++ frontEnd/src/pages/Login.tsx
@@ -0,0 +1,33 @@
+import snowball from '../assets/Snowball.png'
+import CredFrom from '../components/CredFrom'
+
+const Login = () => {
+    return (
+        <>
+            {/*
+              This example requires updating your template:
+      
+              ```
+              <html class="h-full bg-white">
+              <body class="h-full">
+              ```
+            */}
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <img
+                        alt="SnowBall Fin"
+                        src={snowball}
+                        className="mx-auto h-20 w-auto"
+                    />
+                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                        Sign in to your account
+                    </h2>
+                </div>
+            </div>
+            <CredFrom />
+        </>
+    )
+
+}
+
+export default Login
